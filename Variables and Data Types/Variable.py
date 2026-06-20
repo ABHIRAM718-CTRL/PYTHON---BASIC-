e6e0b6fac815c9be18623805c1dc299466d30706
ABHIRAM <bkolapal@student.gitam.edu>
2026-06-20 12:24:07 +0530

    Initial Python practice programs

---
+++ Variables and Data Types/Variable.py
@@ -0,0 +1,7 @@
+name = 'Abhiram'
+age = 19
+gender = "Male"
+print(name)#printing each one in order
+print(age)
+print(gender)
+print(name,age,gender)#printing everything in a single line
---
+++ Variables and Data Types/rulesofvariables.py
@@ -0,0 +1,11 @@
+_ = "abhiram"
+age =19
+AGE = 20
+__ = "language"
+print(_)
+print(age)
+print(AGE)
+print(__)
+# Name must start with a letter or an underscore. It cannot start with a number.
+# Variable names are case sensitive, dont confuse between upper and lower case.
+# You can use Underscore as a name of your variable , but it is not recommended.
