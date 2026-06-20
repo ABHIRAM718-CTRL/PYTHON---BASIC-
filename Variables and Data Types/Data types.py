4316f9a390684bb93e74d757ca1297188a985b21
ABHIRAM <bkolapal@student.gitam.edu>
2026-06-20 17:27:43 +0530

    PRINTING TYPES  AND DATA TYPES

---
+++ Variables and Data Types/Data types.py
@@ -0,0 +1,8 @@
+name = 'Abhiram'
+age = 19
+gender = "Male"
+SHIP = True
+print(type(name))
+print(type(age))
+print(type(gender))# The type function generally tells you the data type of the variable.
+print(type(SHIP))
---
+++ Variables and Data Types/printingtypes.py
@@ -0,0 +1,22 @@
+name = 'Abhiram'
+age = 19
+gender = "Male"
+
+print("My name is", name , "And my Age is", age , "And my Gender is" , gender)
+"""
+first way of printing statements is comma.
+You can mix anything like integer string using commas.
+Also this is multi line comment. You can use triple quotes for multi line comments.
+"""
+print("My name is" + name + " And my Age is" + str(age) + " And my Gender is" + gender)
+"""
+line 12 is second way of printing. see when i given space in string it is giving space between 
+string and variablein output.But in line 11 even i havent given space one space is coming in between.
+ """
+print(f"My name is {name} , my Age is {age} , my Gender is {gender}")
+"""
+This is 3rd way of printing. This is called f-string. 
+You can use variable directly in string using f-string.commas are visible in output.
+you can perform calculations directly in the variable printing.
+"""
+
