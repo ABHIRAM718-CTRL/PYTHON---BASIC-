d4fb1eb7c3284f8f84ac10437064c829d8f6e12d
ABHIRAM <bkolapal@student.gitam.edu>
2026-06-21 08:07:33 +0530

    sep and end operators are done.Also done some assignment questions...

---
+++ Variables and Data Types/ASSIGNMENT/Q1_operations.py
@@ -0,0 +1,11 @@
+"""
+Q1: Take two numbers as input from the user. Print their sum, difference,
+product, and remainder.
+"""
+Gen = int(input("Enter a number: "))
+Rin = int(input("Enter another number: "))
+Sum = Gen+Rin
+Sub = Gen - Rin
+Dot = Gen * Rin
+Rem = Gen % Rin
+print(f"The sum is {Sum},The difference is {Sub},The product is {Dot},The remainder is {Rem}")
---
+++ Variables and Data Types/sep and end.py
@@ -0,0 +1,6 @@
+JOHN = "MY NAME IS"
+DOE = "ABHIRAM"
+print(JOHN, DOE, sep="-")
+#sep is called seperator.used to seperate sentences while printing.
+print(JOHN, DOE, end=".....")
+#end is use to add anything for your print statement at the ends.
