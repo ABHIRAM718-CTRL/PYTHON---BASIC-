113a76a4647fba06b5f88d23bee4056c6cd634e5
ABHIRAM <bkolapal@student.gitam.edu>
2026-06-21 12:11:22 +0530

    Escape sequence and operators started.

---
+++ Escape Sequences and Operators/EscapeSequences.py
@@ -0,0 +1,15 @@
+"""
+\n - next line
+\t - tabspace i.e., 4 consecutive spaces is called tabspace.
+\\ - prints backslash - how many you type, half of them will be printed.
+\' - prints single quote
+\" - prints double quote
+"""
+
+print("MOSFET STANDS FOR\t  - \tMetal Oxide Semiconductor Field Effect Transistor")
+print("BJT STANDS FOR \n - Bipolar Junction Transistor")
+print("BJT STANDS FOR \\\\\\ - \\\\\\Bipolar Junction Transistor") 
+print("BJT STANDS FOR \" - \'Bipolar Junction Transistor")
+
+print("My name is \"Abhiram\"")
+#if you want to print single or double quote,make sure outside ones are not matching that.if matched,use backslash.
---
+++ Escape Sequences and Operators/operators.py
@@ -0,0 +1,9 @@
+G = 6
+N = 7
+print(G+N)#Addition
+print(G-N)#Subtraction
+print(G*N)#Multiplication
+print(G/N)#Division
+print(G//N)#Floor Division
+print(G%N)#Modulus
+print(G**N)#Exponentiation
